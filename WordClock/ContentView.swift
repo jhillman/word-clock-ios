2b9bf345bd8233cb2932ae938c913449a8861162
Jeff Hillman <hillmanapps@gmail.com>
2023-02-13 09:56:00 -0700

    Initial Commit

---
+++ WordClock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WordClock
+//
+//  Created by Jeff Hillman on 2/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordClock/WordClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordClockApp.swift
+//  WordClock
+//
+//  Created by Jeff Hillman on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
