2a76afde7e6244d545ec403b2be7362f3b14940e
Jeff Hillman <hillmanapps@gmail.com>
2023-02-18 17:02:07 -0700

    Initial commit

--- WordClock/ContentView.swift
+++ WordClock/ContentView.swift
@@ -6,16 +6,211 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+import Flow
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
+    
+    @State private var currentTime = ""
+    @State private var ledColor = Color.white
+    @State private var birthday = Date()
+    @State private var updatingBirthday = false
+    private let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    private let timeFormatter = DateFormatter()
+    private let dateFormatter = DateFormatter()
+    private var wordClock = WordClock()
+    
+    let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let date = Date()
+        
+        var startComponents = calendar.dateComponents([.day, .month, .year], from: date)
+        startComponents.day = 1
+        startComponents.month = 1
+        
+        var endComponents = calendar.dateComponents([.day, .month, .year], from: date)
+        endComponents.day = 31
+        endComponents.month = 12
+        
+        return calendar.date(from: startComponents)!...calendar.date(from: endComponents)!
+    }()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(spacing: 8) {
+                HStack(spacing: 8) {
+                    Text("Word Clock connected:")
+                    if wordClock.connected {
+                        SwiftUI.Image(systemName: "checkmark.circle.fill")
+                    } else {
+                        SwiftUI.Image(systemName: "xmark.circle.fill")
+                    }
+                }
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                HStack {
+                    Text("\(currentTime)")
+                        .onReceive(timer) { input in
+                            currentTime = timeFormatter.string(from: Date())
+                        }
+                        .font(Font.system(size: 22))
+                    
+                    Spacer()
+                    
+                    Button {
+                        wordClock.setTime()
+                    } label: {
+                        HStack {
+                            Text("Set time")
+                            SwiftUI.Image(systemName: "clock")
+                        }
+                        .frame(minWidth: 150, maxWidth: 150)
+                        .font(Font.system(size: 18))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 12)
+                        .background(RoundedRectangle(cornerRadius: 10, style: .continuous).fill(Color.blue))
+                    }
+                    .buttonStyle(.plain)
+                    .disabled(!wordClock.connected)
+                }
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                HStack {
+                    ColorPicker("LED color:",
+                                selection: $ledColor)
+                    .frame(maxWidth: 116)
+                    
+                    Spacer()
+                    
+                    Button {
+                        wordClock.setLEDColor(color: ledColor)
+                    } label: {
+                        HStack {
+                            Text("Set color")
+                            SwiftUI.Image(systemName: "sun.max")
+                        }
+                        .frame(minWidth: 150, maxWidth: 150)
+                        .font(Font.system(size: 18))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 12)
+                        .background(RoundedRectangle(cornerRadius: 10, style: .continuous).fill(Color.blue))
+                    }
+                    .buttonStyle(.plain)
+                    .disabled(!wordClock.connected)
+                }
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                VStack(spacing: 8) {
+                    HStack(spacing: 8) {
+                        Text("Birthdays")
+                            .font(Font.system(size: 32))
+                        
+                        if wordClock.updatingBirthdays {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                        }
+                    }
+                    
+                    if wordClock.birthdays.isEmpty {
+                        Text("no birthdays")
+                            .font(Font.system(size: 16))
+                    } else {
+                        HFlow {
+                            ForEach(wordClock.birthdays, id: \.self) { birthday in
+                                HStack(spacing: 8) {
+                                    Text(dateFormatter.string(from: birthday))
+                                        .font(Font.system(size: 14))
+                                    Button {
+                                        wordClock.removeBirthday(date: birthday)
+                                    } label: {
+                                        Image(systemName: "xmark")
+                                            .tint(Color.gray)
+                                    }
+                                    .disabled(!wordClock.connected)
+                                }
+                                .padding(8)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 4)
+                                        .stroke(Color.gray, lineWidth: 2)
+                                )
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    
+                    Spacer()
+                        .frame(height: 24)
+                    
+                    HStack {
+                        Button(dateFormatter.string(from: birthday)) {
+                            updatingBirthday = true
+                        }
+                        .foregroundColor(Color.blue)
+                        .overlay {
+                            DatePicker("",
+                                       selection: $birthday,
+                                       in: dateRange,
+                                       displayedComponents: [.date])
+                            .datePickerStyle(.compact)
+                            .labelsHidden()
+                            .blendMode(.destinationOver)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            wordClock.addBirthday(date: birthday)
+                        } label: {
+                            HStack {
+                                Text("Add birthday")
+                                SwiftUI.Image(systemName: "birthday.cake")
+                            }
+                            .frame(minWidth: 150, maxWidth: 150)
+                            .font(Font.system(size: 18))
+                            .foregroundColor(.white)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 12)
+                            .background(RoundedRectangle(cornerRadius: 10, style: .continuous).fill(Color.blue))
+                        }
+                        .buttonStyle(.plain)
+                        .disabled(!wordClock.connected)
+                    }
+                }
+                
+                Spacer()
+                    .frame(maxHeight: .infinity)
+            }
+            .padding()
+            .onAppear {
+                if let dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: NSLocale.current) {
+                    if dateFormat.contains("a") {
+                        timeFormatter.dateFormat = "H:m:ss a"
+                    } else {
+                        timeFormatter.dateFormat = "HH:mm:ss"
+                    }
+                }
+                
+                currentTime = timeFormatter.string(from: Date())
+                
+                dateFormatter.dateFormat = "MMMM d"
+            }
+            .onChange(of: scenePhase) { phase in
+                if phase == .active {
+                    wordClock.connect()
+                } else {
+                    wordClock.disconnect()
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ WordClock/WordClock.swift
@@ -0,0 +1,212 @@
+//
+//  WordClock.swift
+//  WordClock
+//
+//  Created by Jeff Hillman on 2/13/23.
+//
+
+import Foundation
+import CoreBluetooth
+import SwiftUI
+
+final class WordClock: NSObject, ObservableObject {
+    @Published var connected = false
+    @Published var updatingBirthdays = false
+    @Published var birthdays: [Date] = []
+    
+    private var centralManager: CBCentralManager?
+    private var wordClock: CBPeripheral?
+    private var wordClockCharacteristic: CBCharacteristic?
+    private var color: Color?
+    private var birthday: Date?
+    
+    func connect() {
+        centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+    
+    func disconnect() {
+        if let wordClock = wordClock {
+            centralManager?.cancelPeripheralConnection(wordClock)
+        }
+        
+        wordClock = nil
+        connected = false
+    }
+    
+    func setTime() {
+        if let setTimeData = "settime\n".data(using: .ascii), let characteristic = wordClockCharacteristic {
+            wordClock?.writeValue(setTimeData, for: characteristic, type: .withoutResponse)
+        }
+    }
+    
+    func setLEDColor(color: Color) {
+        if let setTimeData = "setcolor\n".data(using: .ascii), let characteristic = wordClockCharacteristic {
+            self.color = color
+            wordClock?.writeValue(setTimeData, for: characteristic, type: .withoutResponse)
+        }
+    }
+    
+    func listBirthdays() {
+        updatingBirthdays = true
+        
+        if let listBirthdaysData = "listbdays\n".data(using: .ascii), let characteristic = wordClockCharacteristic {
+            wordClock?.writeValue(listBirthdaysData, for: characteristic, type: .withoutResponse)
+        }
+    }
+    
+    func addBirthday(date: Date) {
+        updatingBirthdays = true
+        
+        if let addBirthdayData = "addbday\n".data(using: .ascii), let characteristic = wordClockCharacteristic {
+            self.birthday = date
+            wordClock?.writeValue(addBirthdayData, for: characteristic, type: .withoutResponse)
+        }
+    }
+    
+    func removeBirthday(date: Date) {
+        updatingBirthdays = true
+        
+        if let removeBirthdayData = "removebday\n".data(using: .ascii), let characteristic = wordClockCharacteristic {
+            self.birthday = date
+            wordClock?.writeValue(removeBirthdayData, for: characteristic, type: .withoutResponse)
+        }
+    }
+}
+
+extension WordClock: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: [CBUUID(string: "FFE0")])
+        } else {
+            wordClock = nil
+            connected = false
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name == "WordClock", peripheral.identifier.uuidString == "ACD54B4B-E4A5-739D-37DB-F7D6313547D0" {
+            central.stopScan()
+            peripheral.delegate = self
+            central.connect(peripheral)
+
+            wordClock = peripheral
+            connected = true
+        }
+    }
+}
+
+extension WordClock: CBPeripheralDelegate {
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        wordClock?.discoverServices([CBUUID(string: "FFE0")])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        wordClock = nil
+        connected = false
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else {
+            return
+        }
+        
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else {
+            return
+        }
+        
+        for characteristic in characteristics {
+            if characteristic.uuid.uuidString == "FFE1"  {
+                wordClockCharacteristic = characteristic
+
+                peripheral.setNotifyValue(true, for: characteristic)
+                peripheral.readValue(for: characteristic)
+                
+                listBirthdays()
+                
+                break
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        guard
+            characteristic == wordClockCharacteristic,
+            let characteristicValue = characteristic.value,
+            let characteristicString = String(data: characteristicValue, encoding: .utf8)
+        else { return }
+        
+        let birthdayCountRegex = /Birthday count: (?<count>\d+)/
+        let birthdayRegex = /Birthday (?<number>\d+)\/(?<count>\d+): (?<month>\d+)\/(?<day>\d+)/
+        
+        if characteristicString.starts(with: "Set the date & time") {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy/M/d,HH:mm:ss\n"
+                let time = dateFormatter.string(from: Date())
+                
+                if let timeData = time.data(using: .ascii) {
+                    self.wordClock?.writeValue(timeData, for: characteristic, type: .withoutResponse)
+                }
+            }
+        } else if characteristicString.starts(with: "Set the color"), let color = color {
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+            
+            UIColor(color).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            
+            let ledColor = String(format: "#%02x%02x%02x\n", Int(red * 255), Int(green * 255), Int(blue * 255))
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                if let ledColorData = ledColor.data(using: .ascii) {
+                    self.wordClock?.writeValue(ledColorData, for: characteristic, type: .withoutResponse)
+                }
+            }
+        } else if characteristicString.starts(with: "Enter birthday"), let birthday = birthday {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "M/d\n"
+                let date = dateFormatter.string(from: birthday)
+                
+                if let birthdayData = date.data(using: .ascii) {
+                    self.wordClock?.writeValue(birthdayData, for: characteristic, type: .withoutResponse)
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        self.listBirthdays()
+                    }
+                }
+            }
+        } else if let birthdayCountMatch = characteristicString.firstMatch(of: birthdayCountRegex),
+                  let count = Int(birthdayCountMatch.count) {
+            if count == 0 {
+                DispatchQueue.main.async {
+                    self.updatingBirthdays = false
+                }
+            }
+        } else if let birthdayMatch = characteristicString.firstMatch(of: birthdayRegex),
+                  let number = Int(birthdayMatch.number), let count = Int(birthdayMatch.count),
+                  let month = Int(birthdayMatch.month), let day = Int(birthdayMatch.day),
+                  let birthday = Calendar(identifier: .gregorian).date(from: DateComponents(month: month, day: day)) {
+            DispatchQueue.main.async {
+                if number == 1 {
+                    self.birthdays.removeAll()
+                }
+                
+                self.birthdays.append(birthday)
+                
+                if number == count {
+                    self.birthdays.sort()
+                    self.updatingBirthdays = false
+                }
+            }
+        }
+    }
+}
